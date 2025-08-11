ea96958c6eb7140775060180f8bf39006030476f
Varun Singh <varunsingh7004@gmail.com>
2025-08-11 22:30:12 +0530

    Register page half done offc refinement is left now i will start login page

--- image-kit-project/src/app/register/page.tsx
+++ image-kit-project/src/app/register/page.tsx
@@ -1,9 +1,86 @@
-import React from 'react'
+"use client";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+import { json } from "stream/consumers";
 
 const RegisterPage = () => {
-  return (
-    <div>RegisterPage</div>
-  )
-}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const router = useRouter();
 
-export default RegisterPage
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (password !== confirmPassword) {
+      alert("Password do not match");
+      return;
+    }
+
+    try {
+      const res = await fetch("/api/auth/register", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          email,
+          password,
+        }),
+      });
+      const data = await res.json();
+
+      if (!res.ok) {
+        throw new Error(data.error || "Registration failed");
+      }
+
+      console.log(data);
+      router.push("/login");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  return (
+    // react-query
+    // loading, error, debounce
+    <div>
+      <h1>register</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="confirmPassword">Confirm Password:</label>
+          <input
+            type="password"
+            id="confirmPassword"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Register</button>
+        <p>
+          Already have an account? <a href="/login">Login</a>
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default RegisterPage;
